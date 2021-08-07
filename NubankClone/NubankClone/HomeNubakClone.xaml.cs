537f766d53eb73ab2a596db933ac06750e8ccd00
dieguinsharp <diegoomaispika@outlook.com>
2021-08-07 00:02:31 -0300

    Clone terminado

---
+++ NubankClone/NubankClone/HomeNubakClone.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NubankClone {
+    public partial class HomeNubankClone:ContentPage {
+        public HomeNubankClone () {
+            InitializeComponent();
+        }
+    }
+}
